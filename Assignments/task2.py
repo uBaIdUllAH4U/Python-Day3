cc4c2966f4e9a77d13cfb8aa697b0e2d7ff7eea1
Ubaidullah Waheed Khan <ubaidullahanonymous@gmail.com>
2026-06-07 09:18:30 +0500

    task2.py

--- Assignments/task2.py
+++ Assignments/task2.py
@@ -1,6 +1,6 @@
 name = "Ubaidullah Waheed"
 age = 13
 city = "Karachi"
-print("My name is " + name + ".")
-print("I am " + age + " years old.")
-print("I live in " + city + ".")
+print("My name is ", name, ".")
+print("I am ", age, " years old.")
+print("I live in ", city, ".")
